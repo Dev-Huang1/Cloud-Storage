24347b5a1160b9d5c404d39eae54beb873a28596
Dev.Huang <devhuang000@outlook.com>
2024-07-25 17:00:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -159,4 +159,4 @@ def download(filename):
     return 'File not found', 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='127.0.0.1', port=80, debug=True)
